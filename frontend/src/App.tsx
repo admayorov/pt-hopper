a42641d86a1ad58eb1a57d5f4549265649f80d81
Andrey Mayorov <a.d.mayorov@gmail.com>
2023-03-26 21:05:47 +1100

    Add stop debounce and road names

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,54 +4,68 @@ import { useState } from 'react'
 import './App.css'
 
 function StopTable({ stops }: { stops: Array<any> }) {
-    if (!stops || stops.length === 0) {
-      return null;
-    }
-  
-    return (
-      <table className="table-auto w-full">
-        <thead>
-          <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-            <th className="py-3 px-6 text-left">Stop ID</th>
-            <th className="py-3 px-6 text-left">Stop Name</th>
-            <th className="py-3 px-6 text-left">Suburb</th>
-            <th className="py-3 px-6 text-left">Mode</th>
-          </tr>
-        </thead>
-        <tbody className="text-gray-600 text-sm font-light">
-          {stops.map((stop) => (
-            <tr key={stop.stop_gtfs_id} className="border-b border-gray-200 hover:bg-gray-100">
-              <td className="py-3 px-6 text-left whitespace-nowrap">{stop.stop_gtfs_id}</td>
-              <td className="py-3 px-6 text-left">{stop.name}</td>
-              <td className="py-3 px-6 text-left">{stop.suburb}</td>
-              <td className="py-3 px-6 text-left">{stop.mode}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
+  if (!stops || stops.length === 0) {
+    return null;
   }
-  
+
+  return (
+    <table className="table-auto w-full">
+      <thead>
+        <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+          <th className="py-3 px-6 text-left">Stop ID</th>
+          <th className="py-3 px-6 text-left">Stop Name</th>
+          <th className="py-3 px-6 text-left">Suburb</th>
+          <th className="py-3 px-6 text-left">Mode</th>
+        </tr>
+      </thead>
+      <tbody className="text-gray-600 text-sm font-light">
+        {stops.map((stop) => (
+          <tr key={stop.stop_gtfs_id} className="border-b border-gray-200 hover:bg-gray-100">
+            <td className="py-3 px-6 text-left whitespace-nowrap">{stop.stop_gtfs_id}</td>
+            <td className="py-3 px-6 text-left">
+              {stop.name}
+              <span className="text-xs"><br/>{stop.road_name ? `on ${stop.road_name}` : ""}</span>
+              </td>
+            <td className="py-3 px-6 text-left">{stop.suburb}</td>
+            <td className="py-3 px-6 text-left">{stop.mode}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
 
 
 
 function App() {
   const [query, setQuery] = useState('');
+  const [debounceTimer, setDebounceTimer] = useState<NodeJS.Timeout>();
   const [stops, setStops] = useState([]);
 
-  const handleInputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    let query = event.target.value;
-    setQuery(query);
+  const handleStopSearch = async (query: string) => {
     try {
       const response = await fetch(`${import.meta.env.VITE_BACKEND_HOST}/stops?q=${query}`);
       const data = await response.json();
-      console.log(`Query=${query}. Retrieved ${data.length} stops`);
+      console.log(`Query=${query} retrieved ${data.length} stops`);
       setStops(data)
     } catch (error) {
       console.error(error)
     }
+  };
 
+  const debouncedSearch = (query: string) => {
+    clearTimeout(debounceTimer);
+    let newDebounceTimer = setTimeout(() => {
+      handleStopSearch(query);
+    }, 300);
+    setDebounceTimer(newDebounceTimer)
+  };
 
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value;
+    setQuery(query);
+    debouncedSearch(query);
   };
 
   return (

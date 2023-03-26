421d5b06d4ee419e5c26d95076b42fd9c6ff8a11
Andrey Mayorov <a.d.mayorov@gmail.com>
2023-03-26 16:57:56 +1100

    Add search box to front end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,8 +8,9 @@ function App() {
 
   const [query, setQuery] = useState('');
 
-  const handleSearch = async () => {
-    // Here you can use any library or native fetch method to call the API
+
+  const handleInputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
     const response = await fetch(`/search?q=${query}`);
     const data = await response.json();
     console.log(data);
@@ -17,16 +18,16 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Search by Stop</h1>
-      <div>
-        <input
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Search..."
-        />
-        <button onClick={handleSearch}>Search</button>
-      </div>
+      <h1 className="text-2xl font-bold mb-4">Search by Stop</h1>
+      <div className="flex flex-col items-center">
+        <div className="flex items-center w-full max-w-md">
+          <input className="w-full max-w-md px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-indigo-500 mr-4"
+            type="text"
+            value={query}
+            onChange={handleInputChange}
+            placeholder="Search..."
+          />
+        </div></div>
     </div>
   )
 }

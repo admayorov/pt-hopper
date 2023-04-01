c8bc9736ebfd802853a3e3f7e3529898d6343c9d
Andrey Mayorov <a.d.mayorov@gmail.com>
2023-04-01 12:00:31 +1100

    Move components into folder

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Search from './Search'
-import Departures from './Departures'
-import Map from './Map'
+import Search from './components/Search'
+import Departures from './components/Departures'
+import Map from './components/Map'
 import './index.css'
 
 const router = createBrowserRouter([

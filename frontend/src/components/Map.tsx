252a01094cb443fc03ab81663938609e10cc760a
Andrey Mayorov <a.d.mayorov@gmail.com>
2023-04-10 18:27:59 +1000

    Add labels to stops

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -92,6 +92,73 @@ function Map() {
                             "line-width": 1
                         }
                     },
+
+                    // LABELS
+                    {
+                        "id": 'labels_vline',
+                        "type": 'symbol',
+                        "source": "ptv_stops",
+                        "source-layer": "vline",
+                        "layout": {
+                          'text-field': '{stop_short_name}',
+                          'text-size': 12,
+                          'text-offset': [1, 0],
+                          'text-anchor': 'left'
+                        },
+                        "paint": {
+                          'text-color': '#ffffff'
+                        }
+                    },
+                    {
+                        "id": 'labels_metro',
+                        "type": 'symbol',
+                        "source": "ptv_stops",
+                        "source-layer": "metro",
+                        "layout": {
+                          'text-field': '{stop_short_name}',
+                          'text-size': 10,
+                          'text-offset': [1, 0],
+                          'text-anchor': 'left'
+                        },
+                        "paint": {
+                          'text-color': '#ffffff'
+                        },
+                        "minzoom": 11
+                    },
+                    {
+                        "id": 'labels_tram',
+                        "type": 'symbol',
+                        "source": "ptv_stops",
+                        "source-layer": "tram",
+                        "layout": {
+                          'text-field': '{stop_short_name}',
+                          'text-size': 10,
+                          'text-offset': [1, 0],
+                          'text-anchor': 'left'
+                        },
+                        "paint": {
+                          'text-color': '#ffffff'
+                        },
+                        "minzoom": 13
+                    },
+                    {
+                        "id": 'labels_bus',
+                        "type": 'symbol',
+                        "source": "ptv_stops",
+                        "source-layer": "bus",
+                        "layout": {
+                          'text-field': '{stop_short_name}',
+                          'text-size': 10,
+                          'text-offset': [1, 0],
+                          'text-anchor': 'left'
+                        },
+                        "paint": {
+                          'text-color': '#ffffff'
+                        },
+                        "minzoom": 14
+                    },
+
+                    // STOPS
                     {
                         "id": "bus_stops",
                         "type": "circle",
@@ -101,7 +168,7 @@ function Map() {
                             "circle-color": "#FF8200",
                             "circle-radius": 4
                         },
-                        "minzoom": 13
+                        "minzoom": 14
                     },
                     {
                         "id": "tram_stops",
@@ -112,7 +179,7 @@ function Map() {
                             "circle-color": "#78BE20",
                             "circle-radius": 4
                         },
-                        "minzoom": 11
+                        "minzoom": 13
 
                     },
                     {
@@ -136,6 +203,7 @@ function Map() {
                             "circle-radius": 4
                         }
                     },
+ 
                 ]
                 ,
                 center: [144.93255155345383, -37.82129634139583],
@@ -150,6 +218,17 @@ function Map() {
             console.log(ev.lngLat);
             console.log(map.getZoom())
         })
+
+        for (const layer of ['vline','metro','tram','bus']) {
+            map.on('click', `${layer}_stops`, (ev) => {
+                if (ev.features) {
+                    var feature = ev.features[0];
+                    console.log(feature.properties);
+                }
+            })
+        }
+
+
 
         return () => { map.remove() }
 
